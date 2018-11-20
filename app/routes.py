1e3275edb3ed8996a5dc94690dc5e818be8c7429
Lois <lois.urizar@hpe.com>
2018-11-20 09:32:00 -0800

    COMPLETE Part 7 error handling

--- app/routes.py
+++ app/routes.py
@@ -79,9 +79,9 @@ def before_request():
 @app.route('/edit_profile', methods=['get', 'post'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
-        current_user.username = form.validate_username(form.username.data)
+        current_user.username = form.username.data
         current_user.about_me = form.about_me.data
         db.session.commit()
         flash('Your changes have been saved.')
